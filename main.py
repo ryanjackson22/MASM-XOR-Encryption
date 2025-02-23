dc9c54c2c64796a30562e4ceff287ed74076b438
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-02-23 14:43:34 -0500

    added parameter to xor_encrypt()

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-def xor_encrypt(plain_text):
-    xor_key = 'P'
+# main.py
+
+def xor_encrypt(plain_text: str, key: str):
+    # xor_key = 'P'
     cipher_text = ''
 
     for char in plain_text:
-        integer_to_char = chr(ord(char) ^ ord(xor_key))
+        integer_to_char = chr(ord(char) ^ ord(key))
         cipher_text += integer_to_char
 
     return cipher_text
 
-
 def main():
-    sample_string = "Max Carney"
-
-    encrypted_string = xor_encrypt(sample_string)
-    print("Decrypted String:", encrypted_string)
-
-    decrypted_string = xor_encrypt(encrypted_string)
-    print("Decrypted String:", decrypted_string)
+    pass
 
 if __name__ == "__main__":
    main()
--- xor_test.py
+++ xor_test.py
@@ -1,17 +1,18 @@
+# xor_test.py
+
 import unittest
 
 from main import xor_encrypt
 
-
 class MyTestCase(unittest.TestCase):
     def test_xor_encrypt(self):
         expected = '1(p1">5)'
-        actual = xor_encrypt("Max Carney") # key of 'P'
+        actual = xor_encrypt("Max Carney", 'P')
         self.assertEqual(actual, expected)
 
     def test_xor_decrypt(self):
         expected = "Max Carney"
-        actual = xor_encrypt('1(p1">5)') # key of 'P'
+        actual = xor_encrypt('1(p1">5)', 'P')
         self.assertEqual(actual, expected)
 
 
